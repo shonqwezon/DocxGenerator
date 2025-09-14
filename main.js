f8bdc704f29045f2823af327579b41e996353ed6
Shon Qwezon <shonzbso@gmail.com>
2025-09-14 13:01:53 +0300

    finish

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,      // разрешаем require в рендерере
     },
     autoHideMenuBar: true // скрывает верхнее меню
   })
--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 const { contextBridge } = require('electron')
-
+const path = require("node:path");
+const Docxtemplater = require("docxtemplater");
+const PizZip = require("pizzip");
+const fs = require("fs");
 // expose simple storage API to renderer
 contextBridge.exposeInMainWorld('storage', {
   save: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
@@ -7,4 +10,32 @@ contextBridge.exposeInMainWorld('storage', {
     const data = localStorage.getItem(key)
     return data ? JSON.parse(data) : null
   }
-})
+});
+
+contextBridge.exposeInMainWorld('docxProcessor', {
+  patchFiles: async (files, fields) => {
+    try {
+      for (const fileName of files) {
+        outputDir = path.join(__dirname, 'output')
+        if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+        const content = fs.readFileSync(
+          path.resolve(path.join(__dirname, 'input'), fileName),
+          "binary"
+        );
+        const zip = new PizZip(content);
+        const doc = new Docxtemplater(zip, {
+            paragraphLoop: true,
+            linebreaks: true,
+        });
+        doc.render(fields);
+        const buf = doc.toBuffer();
+        fs.writeFileSync(path.resolve(outputDir, fileName), buf);
+      }
+
+      return true
+    } catch (err) {
+      console.error('Error patching DOCX files:', err)
+      return false
+    }
+  }
+});
